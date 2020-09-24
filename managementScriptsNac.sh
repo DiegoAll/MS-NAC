3ca51afe4dd20199e44c5c89129070ff76db8862
Diego Posada <dposadallano@gmail.com>
2020-09-24 12:03:15 -0500

    Scripts adaptados para funcionar con el menu principal.

--- managementScriptsNac.sh
+++ managementScriptsNac.sh
@@ -14,19 +14,21 @@ do
 echo "(1) Unregister Nodes"
 echo "(2) Server Maintenance"
 echo "(3) SnortG3N"
-echo "(4) Monthly reports"
+echo "(4) Config File Extractor"
 echo "(5) About"
+echo "(6) Documentation"
 echo "(9) Exit"
 echo ""
 echo "      Type the number of the option you want and press enter"
 read opcion
 case $opcion in
 
-1)sh unregNodes.py; exit;;
-2)sh serverMaint.py; exit ;;
-3)python snortG3N; exit;;
-4)sh montlyReports.py; exit ;;
+1)python unregNodes.py; exit;;
+2)sh serverMaint.sh; exit ;;
+3)python snortG3N.py; exit;;
+4)python configFileExtractor.py; exit ;;
 5)python about.py;;
+6)more documentation;;
 9)exit;;
 esac
 sleep 2
